0564922c1f377ed71a20fb1c92dab9b45f6e586b
Pedro Andrade <andradephc11@gmail.com>
2022-08-02 17:03:55 -0300

    H2 database seeding

--- backend/src/main/java/com/client/client/repositories/ClientRepository.java
+++ backend/src/main/java/com/client/client/repositories/ClientRepository.java
@@ -2,6 +2,8 @@ package com.client.client.repositories;
 
 import com.client.client.entitites.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
